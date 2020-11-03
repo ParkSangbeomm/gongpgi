2a50c3001d403aefaced9a51f72789df366e7f47
YeeunJ <21800669@handong.edu>
2020-11-03 12:33:37 +0900

    first setting

---
+++ application/views/welcome_message.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>hello world</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>hello world!</h1>
+
+</body>
+</html>
